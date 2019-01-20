132c7063e3f4dd06fc2e85b373fca5c283031083
Erik Samuelsson <1119465+Samuelsson@users.noreply.github.com>
2019-01-20 20:15:36 +0100

    Add MACOS as layout

--- keyboards/lfkeyboards/lfk78/keymaps/samuelsson/keymap.c
+++ keyboards/lfkeyboards/lfk78/keymaps/samuelsson/keymap.c
@@ -5,7 +5,7 @@
 #define XXXXXXX KC_NO
 
 enum keymap_layout {
-    VANILLA = 0,  // matches MF68 layout
+    MACOS = 0,  // matches MF68 layout
     FUNC,         // 0x02
     SETTINGS,     // 0x04
 };
@@ -23,25 +23,25 @@ const Layer_Info layer_info[] = {
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  /* Keymap VANILLA: (Base Layer) Default Layer
-   * ,---------.  ,------------------------------------------------------------.  ,---------.
-   * | F1 | F2 |  |Esc~| 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |  0| - | = |Backspa|  | Ins|PgUp|
-   * |---------|  |------------------------------------------------------------|  |---------|
-   * | F3 | F4 |  |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|  Ret|  | Del|PgDn|
-   * |---------|  |--------------------------------------------------------.  |  `---------'
-   * | F5 | F6 |  |CAPS   |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|  # |  |
-   * |---------|  |-----------------------------------------------------------|  ,----.
-   * | F7 | F8 |  |Shft| \ |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift     |  | Up |
+  /* Keymap MACOS: (Base Layer) Default Layer
+   * ,---------.  ,------------------------------------------------------------.   ,---------.
+   * | F1 | F2 |  |Esc~| 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |  0| - | = |Backspa|   | Ins|PgUp|
+   * |---------|  |------------------------------------------------------------|   |---------|
+   * | F3 | F4 |  |Tab  | Q | W | E | R | T | Y | U | I | O | P | Å | ¨ |  Ret |   | Del|PgDn|
+   * |---------|  |--------------------------------------------------------.   |   `---------'
+   * | F5 | F6 |  |FUNC   | A | S | D | F | G | H | J | K | L | Ö | Ä | '  |   |
+   * |---------|  |------------------------------------------------------------|   ,----.
+   * | F7 | F8 |  |Shft| \ | Z | X | C | V | B | N | M | , | . | - |Shift      |   | Up |
    * |---------|  |-------------------------------------------------------------------------.
-   * | F9 | F10|  |Ctrl|Win |Alt |      Space            |Alt |Ctrl|Func |    |Lft| Dn |Rig |
-   * `---------'  `------------------------------------------------------'    `-------------'
+   * | F9 | F10|  |Ctrl|Alt |Cmd |      Space            |Cmd |Alt |Ctrl |     |Lft| Dn |Rig |
+   * `---------'  `------------------------------------------------------'     `-------------'
    */
-  [VANILLA] = LAYOUT_iso(
-    KC_F1,  KC_F2,    KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,   KC_BSPC,          KC_INS,  KC_PGUP, \
-    KC_F3,  KC_F4,    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,  KC_ENT,           KC_DEL,  KC_PGDN, \
-    KC_F5,  KC_F6,    KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS,                                      \
-    KC_F7,  KC_F8,    KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,           KC_RSFT,          KC_UP,            \
-    KC_F9,  KC_F10,   KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, KC_RCTL, MO(FUNC),          KC_LEFT, KC_DOWN, KC_RGHT  \
+  [MACOS] = LAYOUT_iso(
+    KC_F1,  KC_F2,    KC_GESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,   KC_BSPC,          KC_INS,  KC_PGUP, \
+    KC_F3,  KC_F4,    KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,  KC_ENT,           KC_DEL,  KC_PGDN, \
+    KC_F5,  KC_F6,    MO(FUNC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS,                                      \
+    KC_F7,  KC_F8,    KC_LSFT,  KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,           KC_RSFT,          KC_UP,            \
+    KC_F9,  KC_F10,   KC_LCTL,  KC_LALT, KC_LGUI,                            KC_SPC,                             KC_RGUI, KC_RALT, KC_RCTL,           KC_LEFT, KC_DOWN, KC_RGHT  \
   ),
 
   /* Keymap FUNCTION: Function Layer
--- keyboards/lfkeyboards/lfk78/keymaps/samuelsson/readme.md
+++ keyboards/lfkeyboards/lfk78/keymaps/samuelsson/readme.md
@@ -1,1 +1,3 @@
 # The default keymap for bluepad
+
+When flashing keyboard the microcontroller for LFK78 revL is `at90usb646`.
