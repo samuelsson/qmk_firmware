b5509a68b6bbb41c67b1e9d449bdb4a3a20718c7
Erik Samuelsson <1119465+Samuelsson@users.noreply.github.com>
2019-03-05 14:13:46 +0100

    Update win layout of lfk78:samuelsson

--- keyboards/lfkeyboards/lfk78/keymaps/samuelsson/keymap.c
+++ keyboards/lfkeyboards/lfk78/keymaps/samuelsson/keymap.c
@@ -67,9 +67,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Keymap WIN: (Second Base Layer) Default Layer for Windows
    * ,---------.  ,-------------------------------------------------------------. ,---------.
-   * | F1 | F2 |  |Esc~| 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |  0| - | = |Backspa | |Ins |PgUp|
+   * | F1 | F2 |  |Esc~| 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |  0| - | = |Backspa | |Ins |Home|
    * |---------|  |-------------------------------------------------------------| |---------|
-   * | F3 | F4 |  |Tab  | Q | W | E | R | T | Y | U | I | O | P | Å | ¨ |  Ent  | |Del |PgDn|
+   * | F3 | F4 |  |Tab  | Q | W | E | R | T | Y | U | I | O | P | Å | ¨ |  Ent  | |Del |End |
    * |---------|  |-------------------------------------------------------.     | `---------'
    * | F5 | F6 |  |FUNC  | A | S | D | F | G | H | J | K | L | Ö | Ä | '  |     |
    * |---------|  |-------------------------------------------------------------| ,----.
@@ -79,8 +79,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `---------'  `------------------------------------------------------'   `--------------'
    */
   [WIN] = LAYOUT_iso(
-    KC_F1,  KC_F2,    KC_GESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,   KC_BSPC,          KC_INS,  KC_PGUP, \
-    KC_F3,  KC_F4,    KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,  KC_ENT,           KC_DEL,  KC_PGDN, \
+    KC_F1,  KC_F2,    KC_GESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,   KC_BSPC,          KC_INS,  KC_HOME, \
+    KC_F3,  KC_F4,    KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,  KC_ENT,           KC_DEL,  KC_END,  \
     KC_F5,  KC_F6,    MO(WFUN), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS,                                      \
     KC_F7,  KC_F8,    KC_LSFT,  KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,           KC_RSFT,          KC_UP,            \
     KC_F9,  KC_F10,   KC_LCTL,  KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, KC_RGUI, KC_RCTL,           KC_LEFT, KC_DOWN, KC_RGHT  \
@@ -88,9 +88,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Keymap FUNCTION: Function Layer for WIN Layer
    * ,---------.  ,-------------------------------------------------------------. ,---------.
-   * |    |    |  | §  |F1 |F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|F11|F12|Delete  | |    |Home|
+   * |    |    |  | §  |F1 |F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|F11|F12|Delete  | |    |PgUp|
    * |---------|  |-------------------------------------------------------------| |---------|
-   * |    |    |  |     |   |Up |   |   |   |   |PgU|Up |PgD|   |   |   |       | |    |End |
+   * |    |    |  |     |   |Up |   |   |   |   |PgU|Up |PgD|   |   |   |       | |    |PgDn|
    * |---------|  |-------------------------------------------------------.     | `---------'
    * |    |    |  |      |Lft|Dwn|Rig|   |   |Hom|Lft|Dwn|Rig|   |   |    |     |
    * |---------|  |-------------------------------------------------------------| ,----.
